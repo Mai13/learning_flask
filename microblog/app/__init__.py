6408e66be2adcce6a1b6438664f2fadfcf5505b1
Mai13 <33417296+Mai13@users.noreply.github.com>
2019-08-05 11:53:31 +0200

    Part 3

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
-from config import Config
+from app.config import Config
+
+#todo: recognize the moluldes made by me
 
 app = Flask(__name__)
 app.config.from_object(Config)
